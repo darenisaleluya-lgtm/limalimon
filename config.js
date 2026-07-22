d3da89415154e1a849cd28e5751810e5cc7f61d7
darenisaleluya-lgtm <darenisaleluya@gmail.com>
2026-07-21 21:08:41 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+window.SURVEY_CONFIG = Object.freeze({
+  OFFICIAL_SITE_URL: "https://darenisaleluya-lgtm.github.io/limalimon/",
+  API_URL: "https://script.google.com/macros/s/AKfycbwj1XAB7JHmZL_7d1qPSuuzZQVMjSt9HGT5rOsO9JwCiTiCC28YwX3krWhhXrRPwI0Q/exec",
+  CAMPAIGN_ID: "demo-001",
+  REQUIRE_GEOLOCATION: true,
+  EXPECTED_MAX_HOURS: 3,
+  AUDIO_BITS_PER_SECOND: 32000,
+  QUESTION_AUDIO_SEGMENT_MS: 300000,
+  FULL_TIMESLICE_MS: 60000,
+  STORAGE_SAFETY_MB: 120,
+  LAST_TEN_PERCENT_ETA_SECONDS: 5,
+  FULL_AUDIO_UPLOAD_LIMIT_MINUTES: 10,
+  FINAL_WAIT_LIMIT_MINUTES: 30,
+  PUBLIC_KEY_JWK: {
+  "kty": "RSA",
+  "n": "tsliR6H0QBR7PvlW32Z2eh8fwXc3yCpYjjHxd-3ZhlWr0RzcBYnFOGwoNBTi7_ES51zbtN1h6CirVdYfQtvEwzMBosHSYOnLm4weJr-FcJRyU1sirx11R-6OUzOixch6CrKNlfhMEEhcCpH7w_W66kbaP3G4t_OMaFHDut3WAjHGrj8_PbYzpGoWfUgL7KhyTT4NhzmzCgJUblzJE4uSd_-dqZO4fzp-9xCdIlG_pLzsdWBBT96JSynj2mOPAwHgC8qMfEU0xwrRuT7mxdxYFn_6r-_w4nF6h_wZHc-QdfWIfc0tZ5skOe7uE0ajCsYzRGnvI-DqCGLtul27mHKqiHU5ynh-bqZv2D_6-7Gn1sMRH_R3HkqYsQ9zZgaZ0USWjtFV0rvwzzJb0-C0OTGdyS-rt5wTJ8IOrWVX1R-J1XpNXekGAIwZ36Xf4540XGFrtqyPj5D0O4O9p8aUoL2Mqqe7tyOu7Gf0dqcN6lWIyQTiq3Iw7lfHAYfrnHCyyvKRNGO8rvVo92kZsySYM6mKrIJ3bII7ibMz93QwUzuTdZoun9NB_ez_6gpdThFnzoJBNxCY-j0qh5P6jPobe81bKd9JRbwq9OdDqwn4EyIDKVDcKV03kJsvjVQgP8l3mZG49qEqXqRWNxSfx0gSyf15qoQBVg5NIjrwHHEoLMgEfVU",
+  "e": "AQAB",
+  "alg": "RSA-OAEP-256",
+  "ext": true,
+  "key_ops": [
+    "encrypt"
+  ],
+  "kid": "encuesta-demo-571f3cdad2f3497b"
+}
+});
